bfc778b681d0b4049833fe5588455f54f41a1326
ChuckChained <84604632+ChuckChained@users.noreply.github.com>
2024-08-26 01:41:36 +0100

    All games added
    Added Tri high, Tri low and Poker. Also changed the run function to loop the specified times additionally to the number already looped.

--- scripts.js
+++ scripts.js
@@ -98,89 +98,210 @@ function rollDice2() {
     toHistory(playersHand.join(" "), dealersHand.join(" "))
 }
 
+function poker() {
+    playersHand.push(rollDice());
+    playersHand.push(rollDice());
+    playersHand.push(rollDice());
+    playersHand.push(rollDice());
+    playersHand.push(rollDice());
+
+    dealersHand.push(rollDice());
+    dealersHand.push(rollDice());
+    dealersHand.push(rollDice());
+    dealersHand.push(rollDice());
+    dealersHand.push(rollDice());
+
+    var pHandSorted = playersHand.sort(function(a, b){return b - a});
+    var dHandSorted = dealersHand.sort(function(a, b){return b - a});
+    console.log(pHandSorted, dHandSorted);
+    //console.log(pokerHandCheck(playersHand));
+    //pokerHandCheck(dealersHand);
+    console.log(pokerHandCheck(pHandSorted));
+    console.log(pokerHandCheck(dHandSorted));
+    var winner = pokerHandCompare(pokerHandCheck(pHandSorted), pokerHandCheck(dHandSorted))
+    updateGameHistory(gameNumber, pHandSorted, pokerHandCheck(pHandSorted)[1], dHandSorted, pokerHandCheck(dHandSorted)[1], winner);
+    updateStats(playerWins, ties, dealerWins);
+    updatePercent();
+    gameNumber++;
+}
+
+function pokerHandCheck(hand) {
+    if(hand[0] == hand[4]) {
+        return [1, "Five of a kind", hand[0]];
+    } else if(hand[0] == hand[3] || hand[1] == hand[4]) {
+        return [2, "Four of a kind", hand[1]];
+    } else if(hand[0] == hand[2] && hand[3] == hand[4]) {
+        return [3, "Strong Full House", hand[0], hand[4]];
+    } else if(hand[0] == hand[1] && hand[2] == hand[4]) {
+        return [4, "Weak Full House", hand[2], hand[0]];
+    } else if(hand[0] == 6 && hand[1] == 5 && hand[2] == 4 && hand[3] == 3 && hand[4] == 2) {
+        return [5, "High Straight"]
+    } else if(hand[0] == 5 && hand[1] == 4 && hand[2] == 3 && hand[3] == 2 && hand[4] == 1) {
+        return [6, "Low Straight"]
+    } else if(hand[0] == hand[2] || hand[1] == hand[3] || hand[2] == hand[4]) {
+        return [7, "Three of a kind", hand[2]]
+    } else {
+        const set = new Set(hand);
+        const duplicates = hand.filter(item => {
+            if(set.has(item)) {
+                set.delete(item);
+                return false;
+            } else {
+                return true;
+            }
+        })
+        if(duplicates.length > 1) {
+            return [8, "Two Pair", duplicates[0], duplicates[1]];
+        } else if(duplicates > 0) {
+            return [9, "One Pair", duplicates[0]];
+        } else {
+            return [10, "Junk"];
+        }
+    }
+}
+
+function pokerHandCompare(hand1, hand2) {
+    if(hand2[0] < hand1[0]) {
+        dealerWins++;
+        return "Dealer";
+    } else if(hand2[0] == hand1[0] && hand2[0] == 1) {
+        if(hand2[2] >= hand1[2]) {
+            dealerWins++;
+            return "Dealer";
+        } else {
+            playerWins++;
+            return "Player";
+        }
+    } else if(hand2[0] == hand1[0] && hand2[0] == 2) {
+        if(hand2[2] >= hand1[2]) {
+            dealerWins++;
+            return "Dealer";
+        } else {
+            playerWins++;
+            return "Player";
+        }
+    } else if(hand2[0] == hand1[0] && hand2[0] == 3) {
+        if(hand2[2] > hand1[2]) {
+            dealerWins++;
+            return "Dealer";
+        } else if(hand1[2] > hand1[2]) {
+            playerWins++;
+            return "Player";
+        } else if(hand2[2] == hand2[2]) {
+            if(hand2[3] >= hand1[3]) {
+                dealerWins++;
+                return "Dealer";
+            } else if(hand2[3] < hand1[3]) {
+                playerWins++;
+                return "Player";
+            }
+        }
+    } else if(hand2[0] == hand1[0] && hand2[0] == 4) {
+        if(hand2[2] > hand1[2]) {
+            dealerWins++;
+            return "Dealer";
+        } else if(hand1[2] > hand1[2]) {
+            playerWins++;
+            return "Player";
+        } else if(hand2[2] == hand2[2]) {
+            if(hand2[3] >= hand1[3]) {
+                dealerWins++;
+                return "Dealer";
+            } else if(hand2[3] < hand1[3]) {
+                playerWins++;
+                return "Player";
+            }
+        }
+    } else if(hand2[0] == hand1[0] && hand2[0] == 5) {
+        dealerWins++;
+        return "Dealer";
+    } else if(hand2[0] == hand1[0] && hand2[0] == 6) {
+        dealerWins++;
+        return "Dealer";
+    } else if(hand2[0] == hand1[0] && hand2[0] == 7) {
+        if(hand2[2] >= hand1[2]) {
+            dealerWins++;
+            return "Dealer";
+        } else if(hand2[2] < hand1[2]) {
+            playerWins++;
+            return "Player";
+        }
+    } else if(hand2[0] == hand1[0] && hand2[0] == 8) {
+        if(hand2[2] > hand1[2]) {
+            dealerWins++;
+            return "Dealer";
+        } else if(hand1[2] > hand1[2]) {
+            playerWins++;
+            return "Player";
+        } else if(hand2[2] == hand2[2]) {
+            if(hand2[3] >= hand1[3]) {
+                dealerWins++;
+                return "Dealer";
+            } else if(hand2[3] < hand1[3]) {
+                playerWins++;
+                return "Player";
+            }
+        }
+    } else if(hand2[0] == hand1[0] && hand2[0] == 9) {
+        if(hand2[2] >= hand1[2]) {
+            dealerWins++;
+            return "Dealer";
+        } else {
+            playerWins++;
+            return "Player";
+        }
+    } else if(hand2[0] == hand1[0] && hand2[0] == 10) {
+        dealerWins++;
+        return "Dealer";
+    } else if(hand1[0] < hand2[0]) {
+        playerWins++;
+        return "Player";
+    }
+}
+
 function rollDice() {
     return getRandomInt(6)+1
 }
 
-function six(rules) {
+function tri(rule) {
     playersHand.push(rollDice());
     playersHand.push(rollDice());
-    while(playersHand.reduce((partialSum, a) => partialSum + a, 0) < 4) {
-        playersHand.push(rollDice());
+    playersHand.push(rollDice());
+
+    dealersHand.push(rollDice());
+    dealersHand.push(rollDice());
+    dealersHand.push(rollDice());
+
+    var playersSum = playersHand.reduce((partialSum, a) => partialSum + a, 0);
+    var dealersSum = dealersHand.reduce((partialSum, a) => partialSum + a, 0);
+
+    if(rule == 'low') {
+        if(playersSum >= dealersSum) {
+            console.log('Dealer Wins');
+            winner = "Dealer";           
+            dealerWins++;
+        } else if (playersSum < dealersSum) {
+            console.log('Player Wins');
+            winner = "Player";
+            playerWins++;
+        }
+    } else if (rule == 'high') {
+        if(playersSum <= dealersSum) {
+            console.log('Dealer Wins');
+            winner = "Dealer";
+            dealerWins++;
+        } else if (playersSum > dealersSum) {
+            console.log('Player Wins');
+            winner = "Player";
+            playerWins++;
+        }
     }
+    updateGameHistory(gameNumber, playersHand, playersSum, dealersHand, dealersSum, winner);
+    updateStats(playerWins, ties, dealerWins);
+    gameNumber++;
+    updatePercent();
+};
 
-    const playersSum = playersHand.reduce((partialSum, a) => partialSum + a, 0);
-    console.log(playersHand);
-    console.log("Players Total: " + playersSum);
-    if(playersSum > 13) {
-        console.log("Player Bust");
-        console.log("Dealer Wins");
-        winner = "Dealer";
-        updateGameHistory(gameNumber, playersHand, playersSum, dealersHand, dealersHand, reduce((partialSum, a) => partialSum + a, 0), winner);
-        updateStats(playerWins, ties, dealerWins)
-    }
-}
-
-// function thirteen(rules) {
-//     playersHand.push(rollDice());
-//     playersHand.push(rollDice());
-//     while(playersHand.reduce((partialSum, a) => partialSum + a, 0) < 11) {
-//         playersHand.push(rollDice());
-//     }
-
-//     const playersSum = playersHand.reduce((partialSum, a) => partialSum + a, 0);
-// //    console.log(playersHand);
-//     console.log(playersHand);
-//     console.log("Players Total: " + playersSum);
-//     if(playersSum > 13) {
-//         console.log("Player Bust")
-//         console.log("Result: Dealer Wins")
-//         winner = "Dealer"
-//         updateGameHistory(gameNumber, playersHand, playersSum, dealersHand, dealersHand.reduce((partialSum, a) => partialSum + a, 0), winner);
-//         updateStats(playerWins, ties, dealerWins);
-//         gameNumber++;
-//         dealerWins++;
-//         //updateStats(playerWins, playerPercent, ties, tiePercent, dealerWins, dealerPercent);
-//         return;
-//     }
-//     dealersHand.push(rollDice());
-//     while(dealersHand.reduce((partialSum, a) => partialSum + a, 0) < playersSum) {
-//         dealersHand.push(rollDice());
-
-//         if(dealersHand.reduce((partialSum, a) => partialSum + a, 0) == playersSum) {
-//             if(rules == "Tie = Redo") {
-//                 console.log("Tie. Redo.");
-//                 winner = "Tie";
-//                 ties++;
-//             } else if (rules == "Tie = Dealer") {
-//                 console.log("Tie. Dealer Wins.");
-//                 winner = "Dealer";
-//                 dealerWins++;
-//             } else if (rules == "Tie = Redo + Ignore Tie Stats") {
-//                 console.log("Ignoring Tie");
-//                 gameNumber--;
-//                 winner = "Tie"
-//             }
-//         } else if (dealersHand.reduce((partialSum, a) => partialSum + a, 0) > 13) {
-//             console.log("Dealer Bust");
-//             console.log("Result: Player Wins");
-//             winner = "Player";
-//             playerWins++;
-//         } else if (dealersHand.reduce((partialSum, a) => partialSum + a, 0) > playersSum) {
-//             console.log("Player: " + playersSum + " Dealer: " + dealersHand.reduce((partialSum, a) => partialSum + a, 0))
-//             console.log("Result: Dealer Wins");
-//             winner = "Dealer";
-//             dealerWins++;
-//         }
-//     } 
-//     console.log(dealersHand)
-//     console.log("Dealers Total: " + dealersHand.reduce((partialSum, a) => partialSum + a, 0));
-//     updateGameHistory(gameNumber, playersHand, playersSum, dealersHand, dealersHand.reduce((partialSum, a) => partialSum + a, 0), winner);
-//     gameNumber++;
-//     updateStats(playerWins, ties, dealerWins);
-//     updatePercent();
-//     console.log("tie percent = " + tiePercent);
-// }
 
 function sixThirteenTwentyone(game, rules, stick) {
     playersHand.push(rollDice());
@@ -244,6 +365,24 @@ function sixThirteenTwentyone(game, rules, stick) {
 }
 
 function reset() {
+    const tableBody = document.getElementById('gameHistory').getElementsByTagName('tbody')[0];
+    tableBody.innerHTML = "<tbody></tbody>";
+
+    p1.innerText = "";
+    d1.innerText = "";
+
+    p2.innerText = "";
+    d2.innerText = "";
+
+    document.getElementById("pp").innerHTML = 0;
+    document.getElementById("tp").innerHTML = 0;
+    document.getElementById("dp").innerHTML = 0;
+    updateStats(0, 0, 0);
+
+    gameNumber = 0;
+    playerWins = 0;
+    dealerWins = 0;
+    ties = 0;
 
 }
 
@@ -252,9 +391,11 @@ function run() {
     var rules = document.getElementById("ruleList");
     var stick = document.getElementById("stickList");
     var numberGamesInput = document.getElementById("numberGamesInput").value;
+    totalGames = Number(totalGames) + Number(numberGamesInput);
+    //var playUntil = totalGames + numberGamesInput;
     console.log("It ran!");
 
-    while(gameNumber - 1 < totalGames + numberGamesInput) {
+    while(gameNumber - 1 < totalGames) {
         playersHand = [];
         dealersHand = [];
         if(gameList.options[gameList.selectedIndex].text == "13" || gameList.options[gameList.selectedIndex].text == "6" || gameList.options[gameList.selectedIndex].text == "21") {
@@ -268,13 +409,22 @@ function run() {
         
         sixThirteenTwentyone(gameList.options[gameList.selectedIndex].text, rules.options[rules.selectedIndex].text, stick.options[stick.selectedIndex].text);
         //thirteen(rules.options[rules.selectedIndex].text);
-    } else {
+        
+    } else if(gameList.options[gameList.selectedIndex].text == "Tri Low") {
+        tri('low')
+    } else if(gameList.options[gameList.selectedIndex].text == "Tri High") {
+        tri('high')
+    } else if(gameList.options[gameList.selectedIndex].text == "Poker") {
+        poker();
+    }
+    else {
         alert("Please select Game 6, 13 or 21");
         return;
     }
     
  //   thirteen();
     }
+//    totalGames += numberGamesInput;
     updateRecentGame();
 //    totalGames = totalGames + numberGamesInput;
 }
